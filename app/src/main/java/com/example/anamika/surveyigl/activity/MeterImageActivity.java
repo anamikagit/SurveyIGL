cc85bdec4cb863c2471bc57abe554d85c48a7e0f
anamikagit <anamika.arya1990@gmail.com>
2018-05-19 13:05:14 +0530

    floating edit text added to login screen

---
+++ app/src/main/java/com/example/anamika/surveyigl/activity/MeterImageActivity.java
@@ -0,0 +1,15 @@
+package com.example.anamika.surveyigl.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.anamika.surveyigl.R;
+
+public class MeterImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meter_image);
+    }
+}
